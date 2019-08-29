afce8613c1b471f2b61f05ae662edac58f6ac94b
Stanislav Pavlov <pavlovstasmail@gmail.com>
2019-08-29 23:38:01 +0300

    add led pwm

--- led_pwm.py
+++ led_pwm.py
@@ -1,10 +1,24 @@
 from machine import Pin, PWM
 from time import sleep
 
-frequency = 5000
-led = PWM(Pin(5), frequency)
 
-while True:
-    for duty_cycle in range(0, 1024):
-        led.duty(duty_cycle)
-        sleep(0.005)
+class LedPwm:
+    """For PWM control of led"""
+    def __init__(self, pin, frequency=5000):
+        self.led = PWM(Pin(pin), frequency)
+        self.led.duty(1024)
+
+    def blink_duty(self, count, interval, duty):
+        """
+            Установить яркость
+            count: Количество раз
+            interval: Интервал в секундах
+            duty: Яркость 1024 - мин, 0 - макс
+        """
+        for i in range(count):
+            # Зажигаю светодиод
+            self.led.duty(duty)
+            sleep(interval)
+            # Гашу светодиод
+            self.led.duty(1024)
+            sleep(interval)
